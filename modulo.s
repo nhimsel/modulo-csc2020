dfa95880da09479ee1c2168051771fdd1121809f
nhimsel <nathanael.himsel@cuw.edu>
2025-02-10 16:45:03 -0600

    convert modulo.c to mips asm

---
+++ modulo.s
@@ -0,0 +1,34 @@
+.text                          # declare where we're gonna place instructions
+main:                          # our main 'class'
+    li   $v0, 5                # load syscall equivalent to scanf("%d", num)
+    syscall                    # execute syscall
+    move $a0, $v0              # save entered value to $a0
+    li   $v0, 5                # load syscall equivalent to scanf("%d", num)
+    syscall                    # execute syscall
+    move $a1, $v0              # save entered value to $a1
+    jal sizeOrder              # call size order
+    jal modulus                # call modulus
+    li $v0, 1                  # load syscall equivalent to printf("%d", num)
+    move $a0, $t0              # move result to $a0 for printing
+    syscall                    # execute syscall
+    li $v0, 10                 # load syscall for exit
+    syscall                    # execute syscall
+
+sizeOrder:                     # sort values $a0 and $a1 by size
+    slt $t0, $a0, $a1          # set $t0 to 1 if $a0 < $a1
+    beq $t0, $zero, return     # if $t0 == 0, they're already sorted
+    move $t0, $a0              # store $a0 temporarily
+    move $a0, $a1              # $a1 is larger, move it to $a0
+    move $a1, $t0              # $a0 is smaller, move it to $a1
+return:
+    jr $ra                     # return
+
+modulus:                       # find $a0 % $a1. $a0 should be larger
+    move $t0, $a0              # copy $a0 to $t0 for the result
+loop:
+    slt $t1, $t0, $a1          # check if $t0 < $a1
+    bne $t1, $zero, end        # if $t0 < $a1, we are done
+    sub $t0, $t0, $a1          # subtract $a1 from $t0
+    j loop                     # repeat the loop
+end:
+    jr $ra                     # return
