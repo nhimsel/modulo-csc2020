c68613304ab145e18c95c7091232e97e6a9bc2ad
nhimsel <nathanael.himsel@cuw.edu>
2025-02-10 11:01:11 -0600

    implement modulo

--- modulo.c
+++ modulo.c
@@ -1,5 +1,56 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+int modulo(int n, int mod);
+
 int main()
 {
+    int a=1;
+    int b=1;
+
+    //get user input
+    printf("%s", "Enter the value:");
+    scanf("%d", &a);
+    printf("%s", "Mod by:");
+    scanf("%d", &b);
+
+    //printf("%d\n", a%b);
+    
+    printf("%d mod %d = ", a, b);
+    printf("%d\n", modulo(a,b));
     return 0;
+}
+
+int modulo(int n, int mod)
+{
+    //should always return the same result as %
+
+    //make n positive for the operations
+    bool wasNeg=false;
+    if (n<0) 
+    {
+        n*=-1;
+        wasNeg=true;
+    }
+
+    //make mod positive for the operations
+    if (mod<0)
+    {
+        mod*=-1;
+    }
+
+    //subtract mod from n until the result would be negative
+    while (n>=mod)
+    {
+        n-=mod;
+    }
+
+    //if n was negative, make the remainder negative
+    if (wasNeg)
+    {
+        n*=-1;
+    }
+
+    //n is the remainder
+    return n;
 }
