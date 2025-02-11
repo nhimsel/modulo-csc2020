c4dad7a59ffc60d56133edcc140b957a71db45ca
nhimsel <nathanael.himsel@cuw.edu>
2025-02-11 08:25:05 -0600

    print strings so we know what to input

--- modulo.s
+++ modulo.s
@@ -1,31 +1,58 @@
+.data                                   # declare where we're gonna place data
+str1:   .asciiz     "Enter dividend: "  # set str1
+str2:   .asciiz     "Enter divisor: "   # set str2
+str3:   .asciiz     " mod "             # set str3   
+str4:   .asciiz     " = "               # set str4
+
 .text                          # declare where we're gonna place instructions
 main:                          # our main 'class'
-    li   $v0, 5                # load syscall equivalent to scanf("%d", num)
+    la $a0, str1               # load str1 into $a0
+    li $v0, 4                  # load syscall equivalent to printf("%s", str)
     syscall                    # execute syscall
-    move $a0, $v0              # save entered value to $a0
-    li   $v0, 5                # load syscall equivalent to scanf("%d", num)
+    li $v0, 5                  # load syscall equivalent to scanf("%d", num)
     syscall                    # execute syscall
     move $a1, $v0              # save entered value to $a1
+    la $a0, str2               # load str2 into $a0
+    li $v0, 4                  # load syscall equivalent to printf("%s", str)
+    syscall                    # execute syscall
+    li $v0, 5                  # load syscall equivalent to scanf("%d", num)
+    syscall                    # execute syscall
+    move $a2, $v0              # save entered value to $a2
     jal sizeOrder              # call size order
+    move $t3, $a1              # copy $a1 to $t3 for later printing
+    move $t4, $a2              # copy $a2 to $t4 for later printing
     jal modulus                # call modulus
+    move $a0, $t3              # load the first value from $t3 to $a0
     li $v0, 1                  # load syscall equivalent to printf("%d", num)
-    syscall                    # execute syscall (answer is already in $a0 for printing)
+    syscall                    # execute syscall
+    la $a0, str3               # load str3 into $a0
+    li $v0, 4                  # load syscall equivalent to printf("%s", str)
+    syscall                    # execute syscall
+    move $a0, $t4              # load the second value from $t4 to $a0
+    li $v0, 1                  # load syscall equivalent to printf("%d", num)
+    syscall                    # execute syscall
+    la $a0, str4               # load str4 into $a0
+    li $v0, 4                  # load syscall equivalent to printf("%s", str)
+    syscall                    # execute syscall    
+    move $a0, $a1              # load our answer from $a1 to $a0
+    li $v0, 1                  # load syscall equivalent to printf("%d", num)
+    syscall                    # execute syscall
     li $v0, 10                 # load syscall for exit
     syscall                    # execute syscall
 
-sizeOrder:                     # sort values $a0 and $a1 by size
-    slt $t0, $a0, $a1          # set $t0 to 1 if $a0 < $a1
+sizeOrder:                     # sort values $a1 and $a2 by size
+    slt $t0, $a1, $a2          # set $t0 to 1 if $a1 < $a2
     beq $t0, $zero, return     # if $t0 == 0, they're already sorted
-    move $t0, $a0              # store $a0 temporarily
-    move $a0, $a1              # $a1 is larger, move it to $a0
-    move $a1, $t0              # $a0 is smaller, move it to $a1
+    move $t0, $a1              # store $a1 temporarily
+    move $a1, $a2              # $a2 is larger, move it to $a1
+    move $a2, $t0              # $a1 is smaller, move it to $a2
 return:
     jr $ra                     # return
 
-modulus:                       # find $a0 % $a1. $a0 should be larger
-    slt $t1, $a0, $a1          # check if $t0 < $a1
-    bne $t1, $zero, end        # if $t0 < $a1, we are done
-    sub $a0, $a0, $a1          # subtract $a1 from $t0
+modulus:                       # find $a1 % $a2. $a1 should be larger
+    slt $t1, $a1, $a2          # check if $t0 < $a2
+    bne $t1, $zero, end        # if $t0 < $a2, we are done
+    sub $a1, $a1, $a2          # subtract $a2 from $t0
     j modulus                  # repeat the loop
 end:
     jr $ra                     # return
